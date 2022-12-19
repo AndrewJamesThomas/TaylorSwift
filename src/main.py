28a98560a284b432ce05a0c08fc3af09d2278a51
AndrewJamesThomas <andrew.ja.thomas@gmail.com>
2022-12-18 22:37:01 -0700

    add message, make into function

--- src/main.py
+++ src/main.py
@@ -1,11 +1,27 @@
-import os
+import datetime
 from twilio.rest import Client
 from secrets import *
 
-client = Client(account_sid, auth_token)
+def main():
+    # Create Message
+    NOW = datetime.datetime.now()
+    TARGET_DATE = datetime.datetime.strptime('Jul 14 2023  6:30PM', '%b %d %Y %I:%M%p')
+    DATE_DIFF = TARGET_DATE - NOW
 
-message = client.messages.create(
-    messaging_service_sid='MG7615276652668de036f743f60301a698',
-    body='Hi',
-    to='+5415880215'
-)
+    DAYS_TO_TARGET = DATE_DIFF.days
+    HOURS_TO_TARGET = round(DATE_DIFF.seconds/60/60)
+    MINUTE_TO_TARGET = round((DATE_DIFF.seconds/60/60))*60) - round((DATE_DIFF.seconds/60)
+
+    MSG = f"Hello! There are {DAYS_TO_TARGET} days, {HOURS_TO_TARGET} hours, and {MINUTE_TO_TARGET} minutes until your Taylor Swift Concert"
+
+    # Send Message
+    PHONE_NUMBER = '+15415880215'
+
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        messaging_service_sid='MG7615276652668de036f743f60301a698',
+        body=MSG,
+        to=PHONE_NUMBER
+    )
+
+    return client
